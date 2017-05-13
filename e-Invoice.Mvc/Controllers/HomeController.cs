6caae779f7088f281bd0904c96149a2724f9e8ff
Kostas Basaris <kbasaris@egritosgroup.gr>
2017-05-13 17:04:27 +0300

    Add Mvc Project

---
+++ e-Invoice.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Invoice.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CreateParastastiko()
+        {
+            return View();
+        }
+
+        public ActionResult UploadPdf()
+        {
+            return View();
+        }
+
+        public ActionResult _UploadPdf()
+        {
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object  
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        //string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";  
+                        //string filename = Path.GetFileName(Request.Files[i].FileName);  
+
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+
+                        
+                            fname = file.FileName;
+                       
+
+                        // Get the complete folder path and store the file inside it.  
+                        fname = Path.Combine(Server.MapPath("~/In/"), fname);
+                        file.SaveAs(fname);
+                    }
+                    // Returns message that successfully uploaded  
+                    return Json("File Uploaded Successfully!");
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error occurred. Error details: " + ex.Message);
+                }
+            }
+
+            return PartialView();
+        }
+
+
+        public ActionResult ViewModal()
+        {
+            return PartialView("_ViewModal");
+        }
+        public ActionResult _ViewModal()
+        {
+            return View();
+        }
+    }
+}
---
+++ e-Invoice.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="e_Invoice.Mvc.MvcApplication" Language="C#" %>
